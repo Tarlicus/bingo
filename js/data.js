9662d72c076607233d5bc7f473c2dce95e3b0cba
Alexander Scheurer <mail@aspepex.net>
2018-10-10 22:34:28 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 
-var headerText = "Bingo";
+var headerText = "Longship Bingo";
 
-var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
+var footerText = "<a href='https://github.com/LongshipStudios/bingo/' target='_blank'>Code available on github</a>";
 
 var winText = "Winner";
 
@@ -10,35 +10,38 @@ var clickSnd = new Audio("audio/click.mp3");
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "Are we the badies?"},
+        {"square": "My Shoe"},
+        {"square": "BRB - Being rather British"},
+        {"square": "WHAT HO FRIENDO!"},
+        {"square": "&lt;DELETED LINK&gt;"},
+        {"square": "will.exe has stopped"},
+        {"square": "Does anybody need a backpack?"},
+        {"square": "Pirate Queen - Little Bear"},
+        {"square": "We're sitll in Subnautica / We never left SOMA"},
+        {"square": "Hero to the people Dan Jones!"},
+        {"square": "How goes the hand?"},
+        {"square": "Dramatic Duck"},
+        {"square": "Twizzlers, Dpangoes and Tea"},
+        {"square": "It's a kaleidoscope!"},
+        {"square": "Tiny Mechs"},
+        {"square": "PORG KET"},
+        {"square": "Mr Viking"},
+        {"square": "How about nooooooo"},
+        {"square": "BRB need tea logo is still up"},
+        {"square": "*some Hellsing Abridged quote*"},
+        {"square": "SPLEEN THRONE"},
+        {"square": "Moose, no! - Moose yes, Moose always yes!"},
+        {"square": "Hotel Scipio"},
+        {"square": "Eat the rich!"},    	
+        {"square": "*Will is sorry for something*"},    	
+        {"square": "*Will makes a weird face*"},    	
+        {"square": "Jumping back into chat"},    	
+        {"square": "*Will plays Chap Hop*"},    	
+        {"square": "CYBER something"},    	
+        {"square": "SHARKS!"},    	
+        {"square": "WHAT APOUT QUEST!?"},    	
+        {"square": "ALL YOU CAN UPGRAD IS STRANTH!?"},    	
+        {"square": "*Will cries*"}  	
     ]
 };
